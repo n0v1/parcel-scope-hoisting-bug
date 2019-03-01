2f333e7ba8105b1c0dd840c0ae3295e1bbe4551d
Christopher Fenn <cfenn@vwd.com>
2019-03-01 13:21:53 +0100

    rename app component to work around variable naming issue

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 import Vue from 'vue';
-import App from '../components/App.vue';
-// import App from '../components/App123.vue';
+import TestApp from '../components/TestApp.vue';
 
 // eslint-disable-next-line vue/no-v-html
 new Vue({
   el: '#app',
-  render: h => h(App),
+  render: h => h(TestApp),
 });
